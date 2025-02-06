95a61278cb71c8844caf8058032bd6672305e1f5
Eliphaz21 <yeabsraandinet19@gmail.com>
2025-02-06 13:56:21 +0300

    Update Anton and Danik.cpp

--- Anton and Danik.cpp
+++ Anton and Danik.cpp
@@ -1,1 +1,31 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int n;
+    string s;
+    cin>>n>>s;
+    int A=0;
+    int D=0;
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='A'){
+            A++;
+        }
+        else{
+            D++;
+        }
+
+    }
+    if(A>D){
+        cout<<"Anton"<<endl;
+
+    }
+    else if(D>A){
+        cout<<"Danik"<<endl;
+    }
+    else{
+        cout<<"Friendship"<<endl;
+    }
+    return 0;
+}
 
